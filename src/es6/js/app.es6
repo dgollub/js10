6dd10299e9922a209c4b97cc9e37b7816faf9fb9
Daniel Kurashige-Gollub <daniel@kurashige-gollub.de>
2015-10-15 23:51:45 +0900

    trying to set up es6 and sass env on cygwin, no success with sass

---
+++ src/es6/js/app.es6
@@ -0,0 +1,5 @@
+/*
+	ES6 code entry point
+*/
+const VERSION = "0.0.1"
+console.log(VERSION);
---
+++ src/es6/sass.rb
@@ -0,0 +1,11 @@
+require "sass"
+options = {
+    :syntax => :scss,
+    :style => :compressed,
+    :sourcemap => :inline,
+    :load_paths => ['./sass/']
+    # load_paths is required in case your 'app.scss' files use @import to attach other scss files 
+}
+
+render = Sass::Engine.new(File.read("scss/app.scss"), options).render
+File.write("dist/app.css", render)
